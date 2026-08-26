29a17aa5a7c6490a29be1d370874ec2968337329
Build-with-Akshit <akshitgupta9359@gmail.com>
2026-08-26 23:13:31 +0530

    Update favicon to use the new big Honey logo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -23,9 +23,9 @@ export const metadata: Metadata = {
     "smart agriculture",
   ],
   icons: {
-    icon: "/icon.svg",
-    shortcut: "/icon.svg",
-    apple: "/icon.svg",
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 };
 
