0ba694664b07ea71701de38f0d82728c8e25f930
Build-with-Akshit <akshitgupta9359@gmail.com>
2026-08-26 19:58:17 +0530

    feat: Add honey pot favicon matching brand design

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -22,6 +22,11 @@ export const metadata: Metadata = {
     "KVIC",
     "smart agriculture",
   ],
+  icons: {
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
